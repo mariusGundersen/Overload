09b62b04900a0cb1203afe6180a5d55f855f15bc
Marius Gundersen <gundersen@gmail.com>
2013-07-29 12:34:49 +0200

    Added specs checking the called arguments and returned result

--- specs/when_chaining_using_with.js
+++ specs/when_chaining_using_with.js
@@ -4,14 +4,15 @@ describe("when chaining using with", function(){
 		spy1,
 		spy2,
 		spy3,
-		spy4;
+		spy4,
+		result;
 
 	beforeEach(function(){
 
-		spy1 = sinon.spy();
-		spy2 = sinon.spy();
-		spy3 = sinon.spy();
-		spy4 = sinon.spy();
+		spy1 = sinon.stub().returns("result1");
+		spy2 = sinon.stub().returns("result2");
+		spy3 = sinon.stub().returns("result3");
+		spy4 = sinon.stub().returns("result4");
 
 		method = overload([String], spy1);
 		method.with([Number], spy2);
@@ -23,7 +24,7 @@ describe("when chaining using with", function(){
 	describe("when called with a string", function(){
 
 		beforeEach(function(){
-			method("hello");
+			result = method("hello");
 		});
 
 		it("should call the first function", function(){
@@ -32,12 +33,21 @@ describe("when chaining using with", function(){
 			expect(spy3.callCount).toBe(0);
 			expect(spy4.callCount).toBe(0);
 		});
+
+		it("should call the function with the passed arguments", function(){
+			expect(spy1.calledWithExactly("hello")).toBe(true);
+		});
+
+		it("should return the result of the spy", function(){
+			expect(result).toBe("result1");
+		});
+
 	});
 
 	describe("when called with a number", function(){
 
 		beforeEach(function(){
-			method(1);
+			result = method(1);
 		});
 
 		it("should call the second function", function(){
@@ -46,12 +56,23 @@ describe("when chaining using with", function(){
 			expect(spy3.callCount).toBe(0);
 			expect(spy4.callCount).toBe(0);
 		});
+
+		it("should call the function with the passed arguments", function(){
+			expect(spy2.calledWithExactly(1)).toBe(true);
+		});
+
+		it("should return the result of the spy", function(){
+			expect(result).toBe("result2");
+		});
 	});
 
 	describe("when called with an array and a function", function(){
 
+		var arg1 = [1, "hello"];
+		var arg2 = function(){};
+
 		beforeEach(function(){
-			method([1, "hello"], function(){});
+			result = method(arg1, arg2);
 		});
 
 		it("should call the third function", function(){
@@ -60,12 +81,22 @@ describe("when chaining using with", function(){
 			expect(spy3.callCount).toBe(1);
 			expect(spy4.callCount).toBe(0);
 		});
+
+		it("should call the function with the passed arguments", function(){
+			expect(spy3.calledWithExactly(arg1, arg2)).toBe(true);
+		});
+
+		it("should return the result of the spy", function(){
+			expect(result).toBe("result3");
+		});
 	});
 
 	describe("when called with something else", function(){
 
+		var arg1 = [1, "hello"];
+
 		beforeEach(function(){
-			method([1, "hello"]);
+			result = method(arg1);
 		});
 
 		it("should call the fourth function", function(){
@@ -73,6 +104,14 @@ describe("when chaining using with", function(){
 			expect(spy2.callCount).toBe(0);
 			expect(spy3.callCount).toBe(0);
 			expect(spy4.callCount).toBe(1);
+		});
+
+		it("should call the function with the passed arguments", function(){
+			expect(spy4.calledWithExactly(arg1)).toBe(true);
+		});
+
+		it("should return the result of the spy", function(){
+			expect(result).toBe("result4");
 		});
 	});
 
