32796d83f9652f28f18be4c89c618dc021aa4bec
Marius Gundersen <gundersen@gmail.com>
2013-07-29 11:13:08 +0200

    inital calling in place

--- source/overload.js
+++ source/overload.js
@@ -1,8 +1,19 @@
 var overload = (function(){
 
 	function overload(){
-		return function(){
-			
+
+		if(arguments.length == 0){
+			throw new Error("usage: overload(function) or overload(array, function)");
+		}else if(arguments.length == 1){
+			var func = arguments[0];
+			return function(){
+				func.apply(this, arguments);
+			};
+		}else if(arguments.length == 2){
+			var func = arguments[1];
+			return function(){
+				func.apply(this, arguments);
+			};
 		}
 	}
 
--- specs/when_calling_overload.js
+++ specs/when_calling_overload.js
@@ -1,17 +1,81 @@
 describe("when calling overload", function(){
 
-	var method;
+	var method,
+		spy;
 
-	beforeEach(function(){
+	beforeEach(function () {
+		spy = sinon.spy();
+	});
 
-		because: {
-			method = overload();
-		}
+
+	describe("with no arguments", function(){
+
+		it("should throw an exception", function(){
+			expect(function(){
+				overload();
+			}).toThrow();
+		});
 
 	});
 
-	it("should return a function", function(){
-		expect(method).toBeA("function");
-	})
+	describe("with one function argument", function(){
+
+		beforeEach(function(){
+
+			because: {
+				method = overload(spy);
+			}
+
+		});
+
+		it("should return a function", function(){
+			expect(method).toBeA("function");
+		});
+
+		describe("when calling the returned function", function(){
+
+			beforeEach(function(){
+				because: {
+					method();
+				}
+			})
+
+			it("should call the spy", function(){
+				expect(spy.callCount).toBe(1);
+			});
+
+		});
+
+	});
+
+	describe("with one array and one function argument", function(){
+
+		beforeEach(function(){
+
+			because: {
+				method = overload([], spy);
+			}
+
+		});
+
+		it("should return a function", function(){
+			expect(method).toBeA("function");
+		});
+
+		describe("when calling the returned function", function(){
+
+			beforeEach(function(){
+				because: {
+					method();
+				}
+			})
+
+			it("should call the spy", function(){
+				expect(spy.callCount).toBe(1);
+			});
+
+		});
+
+	});
 
 });
