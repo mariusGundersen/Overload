01d560505e6216eb3acd824df3892132851307f5
Marius Gundersen <gundersen@gmail.com>
2013-08-20 11:52:09 +0200

    Removing dependencies from this project, adding bower and npm json files for fetching dependencies

--- node_modules/closurecompiler/node_modules/sleep/build.js
+++
@@ -1,8 +0,0 @@
-
-if (process.platform !== 'win32') {
-  require('child_process').exec('node-gyp clean || true; node-gyp configure build', function(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-  });
-}
-
